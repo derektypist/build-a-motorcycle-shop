d7848c9ea220092220d6080ab57e7e11a8c7e50c
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2026-06-04 13:55:50 +0000

    Update

--- dist/src.77de5100.js
+++ dist/src.77de5100.js
@@ -239,7 +239,11 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
+<<<<<<< HEAD
   var ws = new WebSocket(protocol + '://' + hostname + ':' + "36321" + '/');
+=======
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "36869" + '/');
+>>>>>>> aeb900c (Reset to 3106402)
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
