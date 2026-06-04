130e36d770d74fc29d5bb1bd1064716711a8b49f
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2026-06-04 14:00:52 +0000

    Updated package.json

--- dist/index.js
+++ dist/index.js
@@ -193,11 +193,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-<<<<<<< HEAD
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "36321" + '/');
-=======
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "36869" + '/');
->>>>>>> aeb900c (Reset to 3106402)
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "33907" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
